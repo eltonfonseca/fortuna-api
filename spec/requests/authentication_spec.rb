8b1ed2dc3e32cfecbb8e5bd78813a236a0db1007
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2020-08-15 13:56:13 -0300

    Added scenario to authentication test

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -9,35 +9,55 @@ end
 
 RSpec.describe 'Authentication', type: :request do
   describe 'POST /api/v1/login' do
-    let(:user) { create(:user) }
-    let(:params) do
-      {
-        user: {
-          email: user.email,
-          password: user.password
+    context 'when user confirmed email' do
+      let(:user) { create(:user, :confirmed) }
+      let(:params) do
+        {
+          user: {
+            email: user.email,
+            password: user.password
+          }
         }
-      }
+      end
+
+      context 'when params are correct' do
+        before { post api_v1_login_path, params: params }
+
+        it 'returns status ok' do
+          expect(response).to have_http_status :ok
+        end
+
+        it 'returns JTW token in authorization header' do
+          expect(response.headers['Authorization']).to be_present
+        end
+
+        it 'returns valid JWT token' do
+          decoded_token = decoded_jwt_token_from_response(response)
+          expect(decoded_token.first['sub']).to be_present
+        end
+      end
+
+      context 'when login params are incorrect' do
+        before { post api_v1_login_path, params: 'empty' }
+
+        it 'returns unauthorized status' do
+          expect(response).to have_http_status :unauthorized
+        end
+      end
     end
 
-    context 'when params are correct' do
+    context 'when user no confirmed email' do
+      let(:user) { create(:user) }
+      let(:params) do
+        {
+          user: {
+            email: user.email,
+            password: user.password
+          }
+        }
+      end
+
       before { post api_v1_login_path, params: params }
-
-      it 'returns status ok' do
-        expect(response).to have_http_status :ok
-      end
-
-      it 'returns JTW token in authorization header' do
-        expect(response.headers['Authorization']).to be_present
-      end
-
-      it 'returns valid JWT token' do
-        decoded_token = decoded_jwt_token_from_response(response)
-        expect(decoded_token.first['sub']).to be_present
-      end
-    end
-
-    context 'when login params are incorrect' do
-      before { post api_v1_login_path, params: 'empty' }
 
       it 'returns unauthorized status' do
         expect(response).to have_http_status :unauthorized
