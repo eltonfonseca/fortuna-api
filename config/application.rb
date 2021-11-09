726b8875c798826904163685abccd116e65728a6
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2021-11-08 21:41:24 -0300

    added i18n configuration

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module Fortuna
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.locale = :'pt-BR'
+    config.i18n.default_locale = :'pt-BR'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
