73b6f0284036baa42250502535a8dd5c1cf6f05c
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2021-11-08 21:37:39 -0300

    refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: :all
-  devise_scope :user do
-    patch '/api/v1/password', to: 'devise/passwords#update'
-    put '/api/v1/password', to: 'devise/passwords#update'
-    post '/api/v1/password', to: 'devise/passwords#create'
+  root to: 'home#index'
 
-    namespace :api, defaults: { format: :json } do
-      namespace :v1 do
-        get 'confirmation', to: 'users/confirmations#show'
-        post 'login', to: 'users/sessions#create'
-        delete 'logout', to: 'users/sessions#destroy'
-        
-        patch 'signup', to: 'users/registrations#update'
-        put 'signup', to: 'users/registrations#update'
-        delete 'signup', to: 'users/registrations#destroy'
-        post 'signup', to: 'users/registrations#create'
-      end
+  namespace :bff, defaults: { format: :json } do
+    namespace :v1 do
+      post 'authenticate', to: 'auth#login'
+      resources :users
     end
   end
 end
